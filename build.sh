40679de98656598d5c989b23b8122f57e5f36582
Levy A. <levyddsa@gmail.com>
2024-11-06 07:35:30 -0300

    fix: new cross compilation build

--- build.sh
+++ build.sh
@@ -4,16 +4,18 @@ set -xe
 
 cd libsql-c
 
-./build.sh
+cargo zigbuild --target universal2-apple-darwin --features encryption --release
+cargo build --target x86_64-unknown-linux-musl --features encryption --release
+cargo build --target aarch64-unknown-linux-musl --features encryption --release
 
 rm -rf ../lib
 
 mkdir -p \
-  ../lib/aarch64-unknown-linux-gnu \
-  ../lib/x86_64-unknown-linux-gnu \
+  ../lib/aarch64-unknown-linux-musl \
+  ../lib/x86_64-unknown-linux-musl \
   ../lib/universal2-apple-darwin \
 
 cp ./libsql.h ../lib/libsql.h
-cp ./target/x86_64-unknown-linux-gnu/release/liblibsql.so ../lib/x86_64-unknown-linux-gnu/
-cp ./target/aarch64-unknown-linux-gnu/release/liblibsql.so ../lib/aarch64-unknown-linux-gnu/
+cp ./target/x86_64-unknown-linux-musl/release/liblibsql.so ../lib/x86_64-unknown-linux-musl/
+cp ./target/aarch64-unknown-linux-musl/release/liblibsql.so ../lib/aarch64-unknown-linux-musl/
 cp ./target/universal2-apple-darwin/release/liblibsql.dylib ../lib/universal2-apple-darwin/
